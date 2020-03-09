379197900b06d2a28da90fb17854eb96bab3f10b
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-03-10 01:18:14 +0900

    problem5

---
+++ problem5/problem5.ts
@@ -0,0 +1,22 @@
+let minValue: number = 20;
+
+while (!judgment(minValue)) {
+  minValue = minValue + 20;
+}
+
+function judgment(minValue: number): boolean {
+  return (
+    minValue % 11 === 0 &&
+    minValue % 12 === 0 &&
+    minValue % 13 === 0 &&
+    minValue % 14 === 0 &&
+    minValue % 15 === 0 &&
+    minValue % 16 === 0 &&
+    minValue % 17 === 0 &&
+    minValue % 18 === 0 &&
+    minValue % 19 === 0 &&
+    minValue % 20 === 0
+  );
+}
+
+console.log(minValue);
